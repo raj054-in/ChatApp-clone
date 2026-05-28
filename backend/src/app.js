ea145afac4a89f73b93e0cd0cc8fa5d63e023089
Raj Singh <raj.kr.singh.er@gmail.com>
2026-05-28 18:26:33 +0545

    readyForDeployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ app.use("/",(req,res)=>{
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
